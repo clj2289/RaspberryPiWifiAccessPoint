1c00b6c0d235c5778fea1dafbdfc801a1af8f530
clj2289 <clj2289@gmail.com>
2014-10-28 00:00:28 -0400

    added copy files

---
+++ copy_files.sh
@@ -0,0 +1,7 @@
+cd RaspberryPiWifiAccessPoint
+cp hostapd /etc/default/hostapd
+cp hostapd.conf /etc/hostapd/hostapd.conf
+cp interfaces /etc/network/interfaces
+cp udhcpd /etc/default/udhcpd 
+cp dhcpd.conf /etc/udhcpd.conf
+cp config/udhcpd.conf /etc/udhcpd.conf
--- install_master.sh
+++ install_master.sh
@@ -7,3 +7,9 @@ sudo mv hostapd /usr/sbin/hostapd.edimax
 sudo ln -sf /usr/sbin/hostapd.edimax /usr/sbin/hostapd 
 sudo chown root.root /usr/sbin/hostapd 
 sudo chmod 755 /usr/sbin/hostapd
+
+git clone https://github.com/clj2289/RaspberryPiWifiAccessPoint.git
+
+sudo RaspberryPiWifiAccessPoint/copy_files.sh
+
+reboot
