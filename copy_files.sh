910196098061a4a0c5389a41000b109659029cb3
clj2289 <clj2289@gmail.com>
2014-10-28 00:05:33 -0400

    fixed typo

--- copy_files.sh
+++ copy_files.sh
@@ -3,5 +3,4 @@ cp hostapd /etc/default/hostapd
 cp hostapd.conf /etc/hostapd/hostapd.conf
 cp interfaces /etc/network/interfaces
 cp udhcpd /etc/default/udhcpd 
-cp dhcpd.conf /etc/udhcpd.conf
 cp udhcpd.conf /etc/udhcpd.conf
