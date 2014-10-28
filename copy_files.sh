dc09a90ef196ef0444f7b0a8c2dd93f598856e58
clj2289 <clj2289@gmail.com>
2014-10-28 00:04:12 -0400

    had wrong path

--- copy_files.sh
+++ copy_files.sh
@@ -4,4 +4,4 @@ cp hostapd.conf /etc/hostapd/hostapd.conf
 cp interfaces /etc/network/interfaces
 cp udhcpd /etc/default/udhcpd 
 cp dhcpd.conf /etc/udhcpd.conf
-cp config/udhcpd.conf /etc/udhcpd.conf
+cp udhcpd.conf /etc/udhcpd.conf
